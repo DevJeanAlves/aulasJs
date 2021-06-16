99147614dc766b77e4cf27583bd197a5a082ba8f
JeanAlves2021 <jean.pdolsky2@gmail.com>
2021-06-16 01:06:05 -0300

    commit praticando o basico

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// Objects
+
+const person = {
+    name: 'John',
+    age: 28,
+    weight: 88.0,
+    isAdmin: true
+}
+
+console.log(`${person.name} tem ${person.age} anos.`)
+
+// Array
+
+const animals = [
+    "Dog",
+    "Parrot",
+    {
+        name: "Cat",
+        age: 2,
+        weight: 2.7
+    }
+]
+
+console.log(animals[2].name)
